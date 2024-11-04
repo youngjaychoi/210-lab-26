9027a1e695af44f36d0d6da0d7d8abaf73c2a739
youngjaychoi <youngjaychoi02@gmail.com>
2024-11-03 18:56:37 -0800

    commit1

---
+++ lab26.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <chrono>
+#include <vector>
+#include <list>
+#include <set>
+#include <algorithm>
+using namespace std;
+using namespace std::chrono;
+
+template <typename Container>
+void readData(Container &container);
+template <typename Container>
+void insertMid(Container &container, const string &value);
+void insertMid(set<string> &s, const string &value);
+template <typename Container>
+void deleteMid(Container &container);
+void deleteMid(set<string> &s);
+
+int main() {
+    vector<string> vector;
+    list<string> list;
+    set<string> set;
+
+    long long vectorTime = 0, listTime = 0, setTime = 0;
+
+    // READING
+    auto start = high_resolution_clock::now();
+    readData(vector);
+    auto end = high_resolution_clock::now();
+    vectorTime = duration_cast<microseconds>(end - start).count();
+
+    start = high_resolution_clock::now();
+    readData(list);
+    end = high_resolution_clock::now();
+    listTime = duration_cast<microseconds>(end - start).count();
+
+    start = high_resolution_clock::now();
+    readData(set);
+    end = high_resolution_clock::now();
+    setTime = duration_cast<microseconds>(end - start).count();
+
+    cout << right << setw(9) << "Operation" << setw(10) << "Vector" << setw(10) << "List" << setw(10) << "Set" << endl;
+    cout << right << setw(9) << "Read" << setw(10) << vectorTime << setw(10) << listTime << setw(10) << setTime << endl;
+
+    // SORTING
+    start = high_resolution_clock::now();
+    sort(vector.begin(), vector.end());
+    end = high_resolution_clock::now();
+    vectorTime = duration_cast<microseconds>(end - start).count();
+
+    start = high_resolution_clock::now();
+    list.sort();
+    end = high_resolution_clock::now();
+    listTime = duration_cast<microseconds>(end - start).count();
+
+    cout << right << setw(9) << "Sort" << setw(10) << vectorTime << setw(10) << listTime << setw(10) << setTime << endl;
+
+    // INSERTING
+    start = high_resolution_clock::now();
+    insertMid(vector, "TESTCODE");
+    end = high_resolution_clock::now();
+    vectorTime = duration_cast<microseconds>(end - start).count();
+
+    start = high_resolution_clock::now();
+    insertMid(list, "TESTCODE");
+    end = high_resolution_clock::now();
+    listTime = duration_cast<microseconds>(end - start).count();
+
+    start = high_resolution_clock::now();
+    insertMid(set, "TESTCODE");
+    end = high_resolution_clock::now();
+    setTime = duration_cast<microseconds>(end - start).count();
+
+    cout << right << setw(9) << "Insert" << setw(10) << vectorTime << setw(10) << listTime << setw(10) << setTime << setw(10) << endl;
+
+    // DELETING
+    start = high_resolution_clock::now();
+    deleteMid(vector);
+    end = high_resolution_clock::now();
+    vectorTime = duration_cast<microseconds>(end - start).count();
+
+    start = high_resolution_clock::now();
+    deleteMid(list);
+    end = high_resolution_clock::now();
+    listTime = duration_cast<microseconds>(end - start).count();
+
+    start = high_resolution_clock::now();
+    deleteMid(set);
+    end = high_resolution_clock::now();
+    setTime = duration_cast<microseconds>(end - start).count();
+
+    cout << right << setw(9) << "Delete" << setw(10) << vectorTime << setw(10) << listTime << setw(10) << setTime << endl;
+
+    return 0;
+}
+
+template <typename Container>
+void readData(Container &container) {
+    ifstream inFile("codes.txt");
+    string line;
+    while (getline(inFile, line)) {
+        container.insert(container.end(), line);
+    }
+    inFile.close();
+}
+
+template <typename Container>
+void insertMid(Container &container, const string &value) {
+    auto it = container.begin();
+    advance(it, container.size() / 2);
+    container.insert(it, value);
+}
+
+void insertMid(set<string> &s, const string &value) {
+    s.insert(value);
+}
+
+template <typename Container>
+void deleteMid(Container &container) {
+    auto it = container.begin();
+    advance(it, container.size() / 2);
+    container.erase(it);
+}
+
+void deleteMid(set<string> &s) {
+    auto it = s.begin();
+    advance(it, s.size() / 2);
+    s.erase(it);
+}
